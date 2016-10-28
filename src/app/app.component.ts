ce0ebe1c44b73c69b977578e759e88a5acddcdde
neil.lunn <neil.lunn>
2016-10-29 08:48:58 +1100

    up to step 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 @Component({
-  selector: 'app-root',
+  selector: 'mh-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  constructor (private tileService: Title) {
+    this.tileService.setTitle(this.title);
+  }
+
+  onSelect (hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(JSON.stringify(hero));
+  }
+
 }
