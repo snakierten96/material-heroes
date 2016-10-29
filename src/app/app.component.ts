2bbe46667c86787a7545f4d5b252457a9dc15071
neil.lunn <neil.lunn>
2016-10-29 14:39:27 +1100

    resolve service as promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit {
   }
 
   getHeroes (): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
 }
--- src/app/hero/hero.service.ts
+++ src/app/hero/hero.service.ts
@@ -6,8 +6,8 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
 
-  getHeroes (): Hero[] {
-    return HEROES;
+  getHeroes (): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
   }
 
 }
