383ffb04cb9d59ef7534f34e09edaa3d2e62f869
neil.lunn <neil.lunn>
2016-10-29 23:06:28 +1100

    Add functionality to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+import { Hero } from '../heroes';
+import { HeroesService } from '../heroes/services';
+
 @Component({
   selector: 'mh-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,11 +11,17 @@ import { Title } from '@angular/platform-browser';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private titleService: Title) {
+  heroes: Hero[] = [];
+
+  constructor(
+    private titleService: Title,
+    private heroesService: HeroesService) {
     this.titleService.setTitle(['Dashboard','Tour of Heroes'].join(" :: "));
   }
 
-  ngOnInit() {
+  ngOnInit (): void {
+    this.heroesService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1,5));
   }
 
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,12 +5,15 @@ import { MaterialModule } from '@angular/material'
 import { dashboardRouting } from './dashboard.routing';
 import { DashboardComponent } from './dashboard.component';
 
+import { HeroesService } from '../heroes/services';
+
 @NgModule({
   imports: [
     CommonModule,
     dashboardRouting,
     MaterialModule.forRoot()
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent],
+  providers: [HeroesService]
 })
 export class DashboardModule { }
