570313b091b9effe7161b94ba57b99b9b2ad71ac
neil.lunn <neil.lunn>
2016-10-30 01:02:16 +1100

    Initial image display

--- src/app/heroes/services/mock-heroes.ts
+++ src/app/heroes/services/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from '../hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Mr. Nice', image: 'mr-nice.png' },
+  { id: 12, name: 'Narco', image: 'narco.png' },
+  { id: 13, name: 'Bombasto', image: 'bombasto.png' },
+  { id: 14, name: 'Celeritas', image: 'celeritas.png' },
+  { id: 15, name: 'Magneta', image: 'magneta.png' },
+  { id: 16, name: 'RubberMan', image: 'rubber-man.png' },
+  { id: 17, name: 'Dynama', image: 'dynama.png' },
+  { id: 18, name: 'Dr IQ', image: 'dr-iq.png' },
+  { id: 19, name: 'Magma', image: 'magma.png' },
+  { id: 20, name: 'Tornado', image: 'tornado.png' }
 ];
