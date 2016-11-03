db602298217af89d8bcd1a53a00150a68b2b0560
neil.lunn <neil.lunn>
2016-11-03 17:56:42 +1100

    Guard admin child routes

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router,
+         CanActivateChild,
          ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanActivateChild {
 
   constructor(
     private authService: AuthService,
@@ -14,6 +15,10 @@ export class AuthGuardService implements CanActivate {
   canActivate (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     let url: string = state.url;
     return this.checkLogin(url);
+  }
+
+  canActivateChild (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return this.canActivate(route,state);
   }
 
   checkLogin (url: string): boolean {
